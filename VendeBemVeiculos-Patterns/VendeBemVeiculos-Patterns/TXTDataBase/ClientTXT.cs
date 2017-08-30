dae3e91cd12d38ea637ddf8b174051000b2bfb6a
Jallysson Miranda Rocha <jallysson@usp.br>
2017-08-30 15:42:48 -0300

    Criado a classe ClientTXT para manipular o arquivo de clientes

--- VendeBemVeiculos-Patterns/VendeBemVeiculos-Patterns/TXTDataBase/ClientTXT.cs
+++ VendeBemVeiculos-Patterns/VendeBemVeiculos-Patterns/TXTDataBase/ClientTXT.cs
@@ -1,6 +1,32 @@
-﻿namespace VendeBemVeiculos_Patterns
+﻿using System;
+using VendeBemVeiculos_Patterns.Domain;
+
+namespace VendeBemVeiculos_Patterns.TXTDataBase
 {
-    public class ClientTXT
+    public class ClientTXT : TXTDataBase<Person>
     {
+        private const int NAME = 0;
+        private const int RG_DOCUMENT = 1;
+        private const int CPF_DOCUMENT = 2;
+        private const int ADDRESS = 3;
+        private const int PHONE_NUMBER = 4;
+        private const char DELIMITER = ';';
+        protected const string BREAK_LINE = "\r\n";
+
+        protected override string ConvertInstanceToText(Person person)
+        {
+            if (person == null) { throw new NullReferenceException(); }
+            return $"{person.Name}{DELIMITER}{person.RGDocument}{DELIMITER}{person.CPFDocument}{DELIMITER}{person.Address}" +
+                $"{DELIMITER}{person.PhoneNumber}{BREAK_LINE}";
+        }
+
+        protected override Person GetLineContent(string line)
+        {
+            var data = line.Split(DELIMITER);
+            var loadedPerson = (Person)Activator.CreateInstance(typeof(Person), data[NAME], data[CPF_DOCUMENT], data[PHONE_NUMBER]);
+            loadedPerson.RGDocument = data[PHONE_NUMBER];
+            loadedPerson.Address = data[ADDRESS];
+            return loadedPerson;
+        }
     }
 }

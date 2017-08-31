b917b1ff1d817bab0ab08b1c5aa1d37fa02fe600
Gabriel Poltronieri <gabriel.poltr@gmail.com>
2017-08-31 12:15:00 -0300

    Grids e Form

---
+++ VendeBemVeiculos-Patterns/VendeBemVeiculos-Patterns/Grids/GridSale.cs
@@ -0,0 +1,17 @@
+﻿namespace VendeBemVeiculos_Patterns.Grids
+{
+    public class GridSale
+    {
+        public string Customer { get; set; }
+
+        public string Seller { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+    }
+}
---
+++ VendeBemVeiculos-Patterns/VendeBemVeiculos-Patterns/Grids/GridStock.cs
@@ -0,0 +1,15 @@
+﻿namespace VendeBemVeiculos_Patterns.Grids
+{
+    public class GridStock
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
